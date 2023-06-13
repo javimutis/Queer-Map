91d35156302b59b501dc2984f9ed5e08ea57202d
Javiera Mutis <122648544+javimutis@users.noreply.github.com>
2023-06-13 18:33:05 -0400

    Incorporacion de mapa estático

--- app/src/main/java/com/cursoandroid/queermap/MapsActivity.kt
+++ app/src/main/java/com/cursoandroid/queermap/MapsActivity.kt
@@ -1,48 +1,128 @@
 package com.cursoandroid.queermap
 
-
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import com.cursoandroid.queermap.R
-import com.google.android.gms.maps.CameraUpdateFactory
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+    OnMyLocationButtonClickListener, OnMyLocationClickListener {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        val options = GoogleMapOptions()
-        options.mapType(GoogleMap.MAP_TYPE_NORMAL)
-            .compassEnabled(false)
-            .rotateGesturesEnabled(false)
-            .tiltGesturesEnabled(false)
-
-
-        val mapFragment = SupportMapFragment.newInstance(options)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.map, mapFragment)
-            .commit()
-
-        mapFragment.getMapAsync(this)
-    }
-        override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
     }
 
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        googleMap.setOnMyLocationButtonClickListener(this)
+        googleMap.setOnMyLocationClickListener(this)
+
+        // Habilitar la capa Mi ubicación
+        enableMyLocation()
+    }
+
+    private fun enableMyLocation() {
+        if (hasLocationPermission()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                googleMap.isMyLocationEnabled = true
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (isPermissionGranted(
+                    permissions,
+                    grantResults,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) || isPermissionGranted(
+                    permissions,
+                    grantResults,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            ) {
+                enableMyLocation()
+            } else {
+                Toast.makeText(this, "Permiso de ubicación denegado.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Botón de Mi ubicación clickeado", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        val currentPosition = LatLng(location.latitude, location.longitude)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(currentPosition)
+                .title("Mi ubicación actual")
+        )
+    }
+
+    private fun isPermissionGranted(
+        permissions: Array<String>,
+        grantResults: IntArray,
+        permission: String
+    ): Boolean {
+        for (i in permissions.indices) {
+            if (permission == permissions[i]) {
+                return grantResults[i] == PackageManager.PERMISSION_GRANTED
+            }
+        }
+        return false
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 }
-
