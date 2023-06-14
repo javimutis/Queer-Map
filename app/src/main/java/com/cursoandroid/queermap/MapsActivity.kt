8af8e9f7a65e0300489ab5d0630009ff6e51a1e3
Javiera Mutis <122648544+javimutis@users.noreply.github.com>
2023-06-14 19:36:08 -0400

    incorporacion de markers con categorías

--- app/src/main/java/com/cursoandroid/queermap/MapsActivity.kt
+++ app/src/main/java/com/cursoandroid/queermap/MapsActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -11,13 +12,21 @@ import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
+import com.google.android.gms.maps.GoogleMap.OnPoiClickListener
+import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.MapsInitializer.Renderer
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback
+import com.google.android.gms.maps.model.PointOfInterest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
-    OnMyLocationButtonClickListener, OnMyLocationClickListener {
+    OnMyLocationButtonClickListener, OnMyLocationClickListener, OnPoiClickListener,
+    OnMapsSdkInitializedCallback {
 
     private lateinit var googleMap: GoogleMap
 
@@ -25,18 +34,100 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
-    }
+        MapsInitializer.initialize(applicationContext, Renderer.LATEST, this)
 
+        val options = GoogleMapOptions()
+            .mapType(GoogleMap.MAP_TYPE_NORMAL)
+            .compassEnabled(false)
+            .rotateGesturesEnabled(false)
+            .tiltGesturesEnabled(false)
+
+        val mapFragment = SupportMapFragment.newInstance(options)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.map, mapFragment)
+            .commit()
+
+        mapFragment.getMapAsync(this)
+    }
+    data class MarkerCategory(val name: String, val icon: Float)
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
         googleMap.setOnMyLocationButtonClickListener(this)
         googleMap.setOnMyLocationClickListener(this)
+        googleMap.setOnPoiClickListener(this)
 
         // Habilitar la capa Mi ubicación
         enableMyLocation()
-    }
+
+        // Obtener los ajustes de interfaz de usuario del mapa
+        val uiSettings = googleMap.uiSettings
+
+        // Habilitar los controles de zoom
+        uiSettings.isZoomControlsEnabled = true
+
+        // Agregar marcadores con categorías
+        val organization = MarkerCategory("Organizaciones", BitmapDescriptorFactory.HUE_RED)
+        val culture = MarkerCategory("Cultura", BitmapDescriptorFactory.HUE_ORANGE)
+        val health = MarkerCategory("Salud", BitmapDescriptorFactory.HUE_YELLOW)
+        val entertainment = MarkerCategory("Entretenimiento", BitmapDescriptorFactory.HUE_GREEN)
+        val shops = MarkerCategory("Tiendas", BitmapDescriptorFactory.HUE_BLUE)
+        val exploration = MarkerCategory("Exploración", 300f)
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-33.01133938329604, -71.54251642642323))
+                .title("Prevención Viña")
+                .icon(BitmapDescriptorFactory.defaultMarker(organization.icon))
+        )
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-33.04744191035794, -71.60834929999501))
+                .title("Teatro Municipal de Valparaíso")
+                .icon(BitmapDescriptorFactory.defaultMarker(culture.icon))
+        )
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-33.01074603885636, -71.54784261280741))
+                .title("Médico Ginecológico Obstetra -  María Cindy Díaz Díaz")
+                .icon(BitmapDescriptorFactory.defaultMarker(health.icon))
+        )
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-33.043516724940865, -71.61742898440514))
+                .title("Pagano")
+                .icon(BitmapDescriptorFactory.defaultMarker(entertainment.icon))
+        )
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-33.02039838048769, -71.55795668873755))
+                .title("Mo Gastrobar")
+                .icon(BitmapDescriptorFactory.defaultMarker(entertainment.icon))
+        )
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-33.0216340714256, -71.55631179242113))
+                .title("SexShop 'Tentación y deseo'")
+                .icon(BitmapDescriptorFactory.defaultMarker(shops.icon))
+        )
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-33.04500143204479, -71.50136662854236))
+                .title("Zona de cruising en 'Jardín Botánico de Viña del Mar'")
+                .icon(BitmapDescriptorFactory.defaultMarker(exploration.icon))
+        )
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-33.038436316953494, -71.62810017643785))
+                .title("Atención psicologíca, 'Patricia Casanova'")
+                .icon(BitmapDescriptorFactory.defaultMarker(health.icon))
+        )
+            }
 
     private fun enableMyLocation() {
         if (hasLocationPermission()) {
@@ -59,7 +150,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         }
     }
 
-
     private fun hasLocationPermission(): Boolean {
         return (ContextCompat.checkSelfPermission(
             this,
@@ -70,7 +160,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
                     Manifest.permission.ACCESS_COARSE_LOCATION
                 ) == PackageManager.PERMISSION_GRANTED)
     }
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -111,6 +200,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         )
     }
 
+    override fun onPoiClick(poi: PointOfInterest) {
+        Toast.makeText(
+            this,
+            "Clicked: ${poi.name}\nPlace ID: ${poi.placeId}\nLatitude: ${poi.latLng.latitude} Longitude: ${poi.latLng.longitude}",
+            Toast.LENGTH_SHORT
+        ).show()
+
+
+    }
+
     private fun isPermissionGranted(
         permissions: Array<String>,
         grantResults: IntArray,
@@ -122,6 +221,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
             }
         }
         return false
+    }
+
+    override fun onMapsSdkInitialized(renderer: Renderer) {
+        when (renderer) {
+            Renderer.LATEST -> Log.d("MapsDemo", "The latest version of the renderer is used.")
+            Renderer.LEGACY -> Log.d("MapsDemo", "The legacy version of the renderer is used.")
+        }
     }
 
     companion object {
